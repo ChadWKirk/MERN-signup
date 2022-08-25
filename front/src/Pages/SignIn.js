0830c0d5b223f37cf76164d000fc48255017f495
Chad Kirk <ChadWKirk@gmail.com>
2022-08-25 15:44:54 -0500

    edited psuedo code and SignIn page

--- front/src/Pages/SignIn.js
+++ front/src/Pages/SignIn.js
@@ -1,6 +1,16 @@
 import React from "react";
 
 function SignIn() {
-  return <div>this is sign in</div>;
+  return (
+    <div>
+      <form>
+        <label>username</label>
+        <input></input>
+        <label>password</label>
+        <input></input>
+        <button>Sign In</button>
+      </form>
+    </div>
+  );
 }
 export default SignIn;
