1e20a4d48110d328c9c8ba2fa03100da1a831659
Chad Kirk <ChadWKirk@gmail.com>
2022-08-25 16:07:24 -0500

    created SignInForm componen and made email value in mongoose schema in signupTemplate

---
+++ front/src/components/SignInForm.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function SignInForm() {
+  return (
+    <form>
+      <label>username</label>
+      <input></input>
+      <label>password</label>
+      <input></input>
+      <label>email</label>
+      <input></input>
+      <button>Sign Up</button>
+    </form>
+  );
+}
+
+export default SignInForm;
