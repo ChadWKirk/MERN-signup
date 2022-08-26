6668992d602f1f2b4cd0d342d27f8baf5532ba7d
Chad Kirk <ChadWKirk@gmail.com>
2022-08-26 16:25:26 -0500

    got it to save to db

--- front/backend/routes/routes.js
+++ front/backend/routes/routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const signupTemplateCopy = require("../models/signupTemplate");
 
+//when form is submitted, make new model and send to db
 router.post("/SignedUp", (req, res) => {
   const newUser = new signupTemplateCopy({
     username: req.body.username,
@@ -10,6 +11,7 @@ router.post("/SignedUp", (req, res) => {
   });
   newUser.save().then((savedData) => {
     //.save() saves it to db. savedData is the data going to db.
+    //this doesn't work. trying to send react file but it just loads plain html
     res.sendFile(
       "C:/Users/kirkch/Desktop/MERN-Projects/MERN-signup/front/src/Pages/SignedUp.js"
     );
--- front/backend/server.js
+++ front/backend/server.js
@@ -3,18 +3,28 @@ const app = express();
 const mongoose = require("mongoose");
 const router = require("./routes/routes");
 
+//middleware to parse and be able to use req.body
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+//mongoDB connection string. took pw out for security
 const mongoDB =
   "mongodb+srv://ChadKirk:signup@cluster0.bchmycc.mongodb.net/users?retryWrites=true&w=majority";
+
+//connect to db
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+
+//if there's an error connecting to db, console log error message
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
+//testing express at home page
 app.get("/", (req, res) => {
   res.send("test");
 });
 
+//middleware to use routes.js
 app.use("/", router);
+
+//use port 5000 eg: localhost:5000
 app.listen(5000);
