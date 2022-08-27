de013655f1d4d293f5b8fce60be2a133ec94efbc
Chad Kirk <santaomgprod@gmail.com>
2022-08-27 18:17:28 -0500

    made submit function in signup

--- front/src/Pages/SignUp.js
+++ front/src/Pages/SignUp.js
@@ -1,6 +1,29 @@
 import React from "react";
 
 function SignUp() {
+  const [form, setForm] = useState({
+    name: "",
+    password: "",
+    email: "",
+  });
+
+  async function onSubmit(e) {
+    const newUser = { ...form };
+
+    await fetch("http://localhost:5000/SignUp", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(newUser),
+    }).catch((error) => {
+      window.alert(error);
+      return;
+    });
+
+    setForm({ name: "", password: "", email: "" });
+  }
+
   return (
     <div>
       <div>
